677d1ee3c97af8900ea657e10178a61cee143e8b
Robert Parker <rparker@kodama.design>
2019-07-12 13:21:34 -0700

    remove individual button options

--- src/ACF/Fields/Button.php
+++ src/ACF/Fields/Button.php
@@ -16,23 +16,20 @@ class Button extends FieldsBuilder
                     "is-centered" => "<span class=\"dashicons dashicons-editor-aligncenter\"></span>",
                     "is-right" => "<span class=\"dashicons dashicons-editor-alignright\"></span>",
                 ]
-            ])->setDefaultValue('is-left')->setWidth('25')
-            ->addTrueFalse('Remove Spacing')->setWidth('25')
-            ->addTrueFalse('Rounded')->setWidth('25')
+            ])->setDefaultValue('is-left')->setWidth('33')
+            ->addTrueFalse('Remove Spacing')->setWidth('33')
             ->addSelect('Button Size', [
                 'choices' => [
                     'are-small' => 'Small',
                     'are-medium' => 'Normal',
                     'are-large' => 'Large',
                 ],
-            ])->setDefaultValue('are-medium')->setWidth('25')
+            ])->setDefaultValue('are-medium')->setWidth('33')
             ->addRepeater('Buttons')
-                ->addLink('Button')->setWidth('40')
+                ->addLink('Button')->setWidth('50')
                 ->addSelect('Button Color', [
                     'choices' => $button_styles,
-                ])->setDefaultValue($default)->setWidth('20')
-                ->addTrueFalse('Inverted')->setWidth('20')
-                ->addTrueFalse('Outline')->setWidth('20')
+                ])->setDefaultValue($default)->setWidth('50')
             ->endRepeater()
         ;
     }
