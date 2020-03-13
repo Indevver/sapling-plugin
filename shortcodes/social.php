1ddf64144aba5ad1fc1962ad29f682dc3faab52e
Robert Parker <diaboloshogunate@gmail.com>
2020-03-13 11:48:08 -0700

    fix social links

--- shortcodes/social.php
+++ shortcodes/social.php
@@ -3,6 +3,17 @@ use Timber\Timber;
 
 add_shortcode('social', function() {
     $context = Timber::get_context();
+    $context['facebook'] = get_theme_mod('social_facebook');
+    $context['google_plus'] = get_theme_mod('social_google_plus');
+    $context['houzz'] = get_theme_mod('social_houzz');
+    $context['instagram'] = get_theme_mod('social_instagram');
+    $context['linkedin'] = get_theme_mod('social_linkedin');
+    $context['myspace'] = get_theme_mod('social_myspace');
+    $context['pinterest'] = get_theme_mod('social_pinterest');
+    $context['snapchat'] = get_theme_mod('social_snapchat');
+    $context['twitter'] = get_theme_mod('social_twitter');
+    $context['yelp'] = get_theme_mod('social_yelp');
+    $context['youtube'] = get_theme_mod('social_youtube');
 
-    return Timber::compile('leaves/social.twig', $context);
+    return Timber::compile('partials/social.twig', $context);
 });
